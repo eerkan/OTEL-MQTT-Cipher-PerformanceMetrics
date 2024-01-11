a236b4119549c01ae25c25b88f375704e5f7934b
Emre Erkan <emreerkanbilgi2@gmail.com>
2024-01-12 00:11:11 +0300

    removed unused files

--- model/Message.py
+++
@@ -1,7 +0,0 @@
-class Message:
-    correlation_id = None
-    content = None
-
-    def __init__(self, correlation_id, content):
-        self.content = content
-        self.correlation_id = correlation_id
